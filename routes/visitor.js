cb9c47aed546490960f9c3906c744ac15fb3f24d
1460058 <gorasori2@gmail.com>
2021-05-06 15:16:44 +0900

    make visitor.js getVisitorTable, editRemark

--- routes/visitor.js
+++ routes/visitor.js
@@ -6,23 +6,41 @@ const conn = require('../database').init()
 
 
 router.get('/', (req, res) => {
-    const sql = 'SELECT * FROM visitortable';
+    const idx = req.query.idx
 
-    conn.query(sql, (err, row) => {
-        if (err) {
-            console.log(err)
-        } else {
-            datetimeFormat(row)
-            res.json(row)
-        }
-    })
+    if (idx === undefined) {
+        const sql = 'SELECT * FROM visitortable';
+
+        conn.query(sql, (err, row) => {
+            if (err) {
+                console.log(err)
+            } else {
+                datetimeFormat(row)
+                res.json(row)
+            }
+        })
+    } else {
+        const idx1 = Number(idx.split(',')[0])
+        const idx2 = Number(idx.split(',')[1])
+
+        const sql = 'SELECT * FROM visitortable WHERE id >= ' + idx1 + ' AND id < ' + idx2 + ' + 1';
+
+        conn.query(sql, (err, row) => {
+            if (err) {
+                console.log(err)
+            } else {
+                datetimeFormat(row)
+                res.json(row)
+            }
+        })
+    }
 })
 
-router.get('/dateSearch', function(req, res) {
+router.get('/dateSearch', (req, res) => {
     const date = req.query.date;
 
     if (date != undefined && date.length == 10) {
-        conn.query('SELECT * FROM visitortable WHERE time LIKE "' + date + '%"', function(err, row) {
+        conn.query('SELECT * FROM visitortable WHERE time LIKE "' + date + '%"', (err, row) => {
             if (err) {
                 console.log(err)
             } else {
@@ -34,26 +52,50 @@ router.get('/dateSearch', function(req, res) {
         const dateSplit1 = Number(date.split(',')[0].substr(8, 2))
         const dateSplit2 = Number(date.split(',')[1].substr(8, 2))
 
-        if (dateSplit1 < dateSplit2) {
-            conn.query('SELECT * FROM visitortable WHERE time BETWEEN date("' + date.split(',')[0] + '") AND date("' + date.split(',')[1] + '")+1', function(err, row) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    datetimeFormat(row)
-                    res.json(row)
-                }
-            })
+        sql1 = 'SELECT * FROM visitortable WHERE time BETWEEN date("' + date.split(',')[0] + '") AND date("' + date.split(',')[1] + '")+1'
+        sql2 = 'SELECT * FROM visitortable WHERE time BETWEEN date("' + date.split(',')[1] + '") AND date("' + date.split(',')[0] + '")+1'
+
+        const sql = (dateSplit1 < dateSplit2) ? sql1 : sql2
+
+        conn.query(sql, (err, row) => {
+            if (err) {
+                console.log(err)
+            } else {
+                datetimeFormat(row)
+                res.json(row)
+            }
+        })
+    }
+})
+
+router.get('/editRemark/:id', (req, res) => {
+    const idx = req.params.id;
+    const sql = 'SELECT remark FROM visitortable WHERE id = ' + idx;
+
+    conn.query(sql, (err, row) => {
+        if (err) {
+            console.log(err)
         } else {
-            conn.query('SELECT * FROM visitortable WHERE time BETWEEN date("' + date.split(',')[1] + '") AND date("' + date.split(',')[0] + '")+1', function(err, row) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    datetimeFormat(row)
-                    res.json(row)
-                }
+            res.send(row[0].remark)
+        }
+    })
+})
+
+router.patch('/editRemark/:id', (req, res) => {
+    const idx = req.params.id;
+    const remark_text = req.body.remark_text
+
+    const sql = 'UPDATE visitortable SET remark = "' + remark_text + '" WHERE id=' + idx;
+    conn.query(sql, (err, row) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json({
+                success: true,
+                code: 200
             })
         }
-    }
+    })
 })
 
 function datetimeFormat(row) {
