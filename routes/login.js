97c4fa25edba83796e25168d745cb96885acb90e
1460058 <gorasori2@gmail.com>
2021-05-18 19:39:33 +0900

    change database, make function dataFormat

--- routes/login.js
+++ routes/login.js
@@ -1,32 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const cryptoJS = require('crypto-js');
-const secret = require('../config').KEY
+const jwt = require("jsonwebtoken");
+const cryptoJS = require("crypto-js");
+const secret = require("../config").KEY;
 
-const conn = require('../database').init()
+const conn = require("../database").init();
 
 router.get("/", (req, res) => {
-    const token = req.headers['access-token']
+    const token = req.headers["access-token"];
     let the_secret_key = "the_secret_key";
 
     jwt.verify(token, the_secret_key, (err, toekn_res) => {
-        if (err) console.log(err)
+        if (err) console.log(err);
         else {
             res.json({
-                'id': toekn_res.id,
-                'name': toekn_res.name,
-                'email': toekn_res.email,
-            })
+                id: toekn_res.id,
+                name: toekn_res.name,
+                email: toekn_res.email,
+            });
         }
-    })
-})
+    });
+});
 
 router.post("/", (req, res) => {
     const userInfo = {
-        'email': req.body.email,
-        'password': req.body.password
-    }
+        email: req.body.email,
+        password: req.body.password,
+    };
     const sql = 'SELECT * FROM users WHERE email = "' + userInfo.email + '"';
     conn.query(sql, (err, row) => {
         if (row[0] !== undefined && row[0].email == userInfo.email) {
@@ -34,27 +34,29 @@ router.post("/", (req, res) => {
             var pw_decrypted = JSON.parse(pw_bytes.toString(cryptoJS.enc.Utf8));
             if (pw_decrypted == userInfo.password) {
                 const token = jwt.sign({
-                    id: row[0].idx,
-                    name: row[0].name,
-                    email: row[0].email
-                }, "the_secret_key");
+                        id: row[0].id,
+                        name: row[0].name,
+                        email: row[0].email,
+                    },
+                    "the_secret_key"
+                );
                 res.json({ token });
             } else {
                 res.json({
-                    'status': 400,
-                    'message': "비밀번호를 확인해주세요."
-                })
+                    status: 400,
+                    message: "비밀번호를 확인해주세요.",
+                });
             }
         } else {
             res.json({
-                'status': 400,
-                'message': "이메일과 비밀번호를 확인해주세요."
-            })
+                status: 400,
+                message: "이메일과 비밀번호를 확인해주세요.",
+            });
         }
         if (err) {
-            console.log(err)
+            console.log(err);
         }
-    })
+    });
 });
 
 // // MIDDLEWARE
--- routes/visitor.js
+++ routes/visitor.js
@@ -1,92 +1,95 @@
-const express = require('express');
-const dateFormat = require('dateformat');
-const e = require('express');
+const express = require("express");
+const dateFormat = require("dateformat");
 
 const router = express.Router();
-const conn = require('../database').init()
+const conn = require("../database").init();
 
+router.get("/", (req, res) => {
+    const idx = req.query.idx;
 
-router.get('/', (req, res) => {
-    const idx = req.query.idx
-
-    if (idx === undefined) {
-        const sql = 'SELECT * FROM visitortable';
+    if (idx == undefined) {
+        const sql = "SELECT * FROM faceInfo;";
 
         conn.query(sql, (err, row) => {
             if (err) {
-                console.log(err)
+                console.log(err);
             } else {
-                datetimeFormat(row)
-                res.json(row)
+                dataFormat(row)
+                res.json(row);
             }
-        })
+        });
     } else {
-        const idx1 = Number(idx.split(',')[0])
-        const idx2 = Number(idx.split(',')[1])
+        const idx1 = Number(idx.split(",")[0]);
+        const idx2 = Number(idx.split(",")[1]);
 
-        const sql = 'SELECT * FROM visitortable WHERE id >= ' + idx1 + ' AND id < ' + idx2 + ' + 1';
+        const sql =
+            "SELECT * FROM faceInfo WHERE id >= " + idx1 + " AND id < " + idx2 + "+ 1";
 
         conn.query(sql, (err, row) => {
             if (err) {
-                console.log(err)
+                console.log(err);
             } else {
-                datetimeFormat(row)
-                res.json(row)
+                dataFormat(row)
+                res.json(row);
             }
-        })
+        });
     }
-})
+});
 
-router.get('/dateSearch', (req, res) => {
+router.get("/dateSearch", (req, res) => {
     const date = req.query.date;
 
     if (date != undefined && date.length == 10) {
-        conn.query('SELECT * FROM visitortable WHERE time LIKE "' + date + '%"', (err, row) => {
-            if (err) {
-                console.log(err)
-            } else {
-                datetimeFormat(row)
-                res.json(row)
+        conn.query(
+            'SELECT * FROM faceInfo WHERE created_at LIKE "' + date + '%"',
+            (err, row) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    dataFormat(row)
+                    res.json(row);
+                }
             }
-        })
+        );
     } else {
-        const dateSplit1 = Number(date.split(',')[0].substr(8, 2))
-        const dateSplit2 = Number(date.split(',')[1].substr(8, 2))
+        const dateSplit1 = Number(date.split(",")[0].substr(8, 2));
+        const dateSplit2 = Number(date.split(",")[1].substr(8, 2));
 
-        sql1 = 'SELECT * FROM visitortable WHERE time BETWEEN date("' + date.split(',')[0] + '") AND date("' + date.split(',')[1] + '")+1'
-        sql2 = 'SELECT * FROM visitortable WHERE time BETWEEN date("' + date.split(',')[1] + '") AND date("' + date.split(',')[0] + '")+1'
+        sql1 = 'SELECT * FROM faceInfo WHERE created_at BETWEEN date("' + date.split(",")[0] + '") AND date("' + date.split(",")[1] + '")+1';
+        sql2 = 'SELECT * FROM faceInfo WHERE created_at BETWEEN date("' + date.split(",")[1] + '") AND date("' + date.split(",")[0] + '")+1';
 
-        const sql = (dateSplit1 < dateSplit2) ? sql1 : sql2
+        const sql = dateSplit1 < dateSplit2 ? sql1 : sql2;
 
         conn.query(sql, (err, row) => {
             if (err) {
-                console.log(err)
+                console.log(err);
             } else {
-                datetimeFormat(row)
-                res.json(row)
+                dataFormat(row)
+                res.json(row);
             }
-        })
+        });
     }
-})
+});
 
-router.get('/editRemark/:id', (req, res) => {
+router.get("/editRemark/:id", (req, res) => {
     const idx = req.params.id;
-    const sql = 'SELECT id, remark FROM visitortable WHERE id = ' + idx;
+    const sql = "SELECT id, remark FROM faceInfo WHERE id = " + idx;
 
     conn.query(sql, (err, row) => {
         if (err) {
-            console.log(err)
+            console.log(err);
         } else {
-            res.send(row[0])
+            res.send(row[0]);
         }
-    })
-})
+    });
+});
 
-router.patch('/editRemark/:id', (req, res) => {
+router.patch("/editRemark/:id", (req, res) => {
     const idx = req.params.id;
     const remark_text = req.body.remark_text;
 
-    const sql = 'UPDATE visitortable SET remark = "' + remark_text + '" WHERE id=' + idx;
+    const sql =
+        'UPDATE faceInfo SET remark = "' + remark_text + '" WHERE id=' + idx;
     conn.query(sql, (err) => {
         if (err) {
             console.log(err);
@@ -94,11 +97,11 @@ router.patch('/editRemark/:id', (req, res) => {
             console.log("success remark update");
             res.sendStatus(200);
         }
-    })
-})
+    });
+});
 
-router.delete('/delete', (req, res) => {
-    const sql = 'DELETE FROM visitortable';
+router.delete("/delete", (req, res) => {
+    const sql = "DELETE FROM faceInfo";
 
     conn.query(sql, (err) => {
         if (err) {
@@ -107,13 +110,13 @@ router.delete('/delete', (req, res) => {
             console.log("success all delete");
             res.sendStatus(200);
         }
-    })
-})
+    });
+});
 
-router.delete('/delete/:id', (req, res) => {
+router.delete("/delete/:id", (req, res) => {
     const idx = req.params.id;
 
-    const sql = 'DELETE FROM visitortable WHERE id = ' + idx;
+    const sql = "DELETE FROM faceInfo WHERE id = " + idx;
     conn.query(sql, (err) => {
         if (err) {
             console.log(err);
@@ -121,14 +124,15 @@ router.delete('/delete/:id', (req, res) => {
             console.log("success selected delete");
             res.sendStatus(200);
         }
-    })
-})
+    });
+});
 
-function datetimeFormat(row) {
+function dataFormat(row) {
     for (key in row) {
-        row[key].date = dateFormat(row[key].date, "yyyy-mm-dd")
-        row[key].time = dateFormat(row[key].time, "HH:MM:ss")
+        row[key].date = dateFormat(row[key].created_at, "yyyy-mm-dd");
+        row[key].time = dateFormat(row[key].created_at, "HH:MM:ss");
+        row[key].age = row[key].ageRange_low + " ~ " + row[key].ageRange_high;
     }
 }
 
-module.exports = router
+module.exports = router;
