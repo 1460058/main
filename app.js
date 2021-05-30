cdf45dd4e331d9c531da650ef22dd7b65c09c4e6
1460058 <gorasori2@gmail.com>
2021-05-30 12:51:02 +0900

    make register.js

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/", require("./routes/index"));
+app.use("/register", require("./routes/register"));
 app.use("/login", require("./routes/login"));
-app.use("/visitor", require("./routes/visitor"));
-app.get("/dashboard", (req, res) => {
-  console.log("this is dashboard api");
-});
+app.use("/dashboard", require("./routes/dashboard"));
+app.use("/faceInfo", require("./routes/faceInfo"));
+app.use("/visitorList", require("./routes/visitorList"));
 
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
